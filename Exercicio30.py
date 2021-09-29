7d0c2bdf24a438769c33736b41f9d0fd0fae5cc5
Emmanuel Siqueira <91622096+EmmaWok@users.noreply.github.com>
2021-09-29 13:51:52 -0300

    Exercicio

---
+++ Exercicio30.py
@@ -0,0 +1,8 @@
+print("digite abaixo o valor em real e em seguida a cotação atual do dolar:")
+real = float(input())
+
+print('digite agora a cotação atual do dolar')
+dolar = float(input())
+
+cot = real/dolar
+print(f'de acordo com a cotação :{cot:.2f} dolares')
