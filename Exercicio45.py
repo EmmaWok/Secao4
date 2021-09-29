a06dd4dd40f5eade9043ffd9d019b7feae0fcb3f
Emmanuel Siqueira <91622096+EmmaWok@users.noreply.github.com>
2021-09-29 13:59:26 -0300

    Exercicio

---
+++ Exercicio45.py
@@ -0,0 +1,8 @@
+print('Digite uma letra maiúscula:')
+Maiu = input()
+ordASCIIMa = ord(Maiu)
+
+ordASCIIMinu = ord(Maiu) +32
+Minuscula = chr(ordASCIIMinu)
+
+print(f'A letra em minúsculo é: {Minuscula}')
