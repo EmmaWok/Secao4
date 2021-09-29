73556b3de87399df8df65f961988da25d79e9198
Emmanuel Siqueira <91622096+EmmaWok@users.noreply.github.com>
2021-09-29 13:52:49 -0300

    Exercicio

---
+++ Exercicio32.py
@@ -0,0 +1,8 @@
+print("Digite um numero inteiro")
+num = int(input())
+
+triplo = (num * 3) + 1
+dobro = (num * 2) - 1
+
+print(f"Sucessor do seu triplo é {triplo}")
+print(f"Atencessor do seu dobro é {dobro}")
