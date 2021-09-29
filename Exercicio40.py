2495546dc7100cd4ec4b73bd8e6da6b437b628d6
Emmanuel Siqueira <91622096+EmmaWok@users.noreply.github.com>
2021-09-29 13:57:07 -0300

    Exercicio

---
+++ Exercicio40.py
@@ -0,0 +1,5 @@
+print('Insira abaixo os dias trabalhados:')
+dias_trabalhados = int(input())
+dia_de_trabalho = 30.0
+quantia_a_receber = dia_de_trabalho * dias_trabalhados * 0.92
+print(f'O valor em liquido que deve pagar para o encanador é de: {quantia_a_receber}')
