f1e49e41ef2ce4d13456b31b58df5ce0b365a49e
Emmanuel Siqueira <91622096+EmmaWok@users.noreply.github.com>
2021-09-29 13:50:07 -0300

    Exercicio

---
+++ Exercicio28.py
@@ -0,0 +1,15 @@
+print('Digite abaixo três valores:')
+valor1 = int(input())
+vlr1 = valor1 **2
+print(vlr1)
+
+valor2 = int(input())
+vlr2 = valor2 **2
+print(vlr2)
+
+valor3 = int(input())
+vlr3 = valor3 **2
+print(vlr3)
+
+soma = vlr1 + vlr2 + vlr3
+print(f'a soma dos três valores é igual :{soma} ')
