ee6ebff398139cdff4d39538a26fdf9b60985f7f
Emmanuel Siqueira <91622096+EmmaWok@users.noreply.github.com>
2021-09-29 13:37:18 -0300

    Exercicio

---
+++ Exercicio12.py
@@ -0,0 +1,6 @@
+
+print('Bem vindo à conversão de Milhas para Quilómetros')
+print('Digite a velocidade em Milhas(MPH) desejada:')
+M = float(input())
+K = 1.61*(M)
+print(f'{M} Milhas por hora equivale há: {K} km/h')
