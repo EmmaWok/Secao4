2027e1d68f293698f06d90ee2ba53d13d7eb228d
Emmanuel Siqueira <91622096+EmmaWok@users.noreply.github.com>
2021-09-29 13:50:58 -0300

    Exercicio

---
+++ Exercicio29.py
@@ -0,0 +1,8 @@
+print("digite 4 valores abaixo")
+valor1 = float(input())
+valor2 = float(input())
+valor3 = float(input())
+valor4 = float(input())
+soma = valor1 + valor2 + valor3 + valor4
+media = soma/4
+print(f'a média aritmética dos valores informados é: {media}')
